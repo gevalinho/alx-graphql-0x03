628bfc0392c793dfa10983a5baa6aa716651bb26
gevalinho <gevalinho1@gmail.com>
2025-10-22 13:16:11 +0100

    Monitor and Log Errors

---
+++ sentry.client.config.ts
@@ -0,0 +1,7 @@
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  tracesSampleRate: 1.0, // adjust as needed
+  environment: process.env.NODE_ENV,
+});
